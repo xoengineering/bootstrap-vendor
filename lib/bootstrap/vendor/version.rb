a83ff2ae895b3c90eccc2c8ce2ee2c0225dec82a
Shane Becker <veganstraightedge@gmail.com>
2026-03-24 17:54:18 -0700

    Initial gem skeleton from bundle gem

---
+++ lib/bootstrap/vendor/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bootstrap
+  module Vendor
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/bootstrap/vendor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bootstrap::Vendor do
+  it "has a version number" do
+    expect(Bootstrap::Vendor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
